4d97ea000651a67afa665bec8a52c70830b848af
Young-Ho Kim <yghokim@younghokim.net>
2025-01-27 23:57:36 +0900

    added aacesstalk demo video loop.

--- app/sections/Demo.tsx
+++ app/sections/Demo.tsx
@@ -1,15 +1,9 @@
-import Lottie from 'react-lottie';
-
 const animData = require('../../public/settings-anim.json')
 
-const lottieOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animData
-}
-
 export const Demo = () => {
-    return <div className="mt-10 rounded-xl overflow-hidden">
-        <Lottie options={lottieOptions}/>
-    </div>
+    return <div className="mt-10 rounded-xl overflow-hidden border border-4 border-orange-600/10 aspect-video">
+    <video className="teaser-loop" autoPlay loop muted playsInline>
+                    <source src={require('../../public/aacesstalk_demo_loop.mp4')} type="video/mp4" />
+                </video>
+</div> 
 }
