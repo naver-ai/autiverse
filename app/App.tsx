9e6875af1059be703dccbd8829499b7b093785b0
Young-Ho Kim <yghokim@younghokim.net>
2025-03-28 16:44:42 +0900

    Mentioned a best paper award.

--- app/App.tsx
+++ app/App.tsx
@@ -5,12 +5,16 @@ import { Abstract } from "./sections/Abstract";
 import { Footer } from "./sections/Footer";
 import { Demo } from "./sections/Demo";
 import { Video } from "./sections/Video";
+import { TrophyIcon } from "@heroicons/react/20/solid";
 
 export function App() {
   return (<>
     <main className="container mx-auto px-4 sm:px-12 pt-6">
       <div>
-        <div className="font-bold text-lg text-[#4592ec] mb-3">CHI 2025 Paper</div>
+        <div className="font-bold text-lg text-[#4592ec] mb-3 flex items-center gap-2">
+          <TrophyIcon className="w-6 h-6 text-[#4592ec]"/>
+          <span>CHI 2025 Best Paper Award</span>
+        </div>
         <img className="mb-2 h-[95px] ml-[-30px] md:h-[110px] md:ml-[-37px]" src={require("../public/images/logos/aacesstalk-logo.svg")}/> 
         <div className="font-light leading-8 sm:leading-[2.5rem] text-2xl sm:text-3xl">Fostering Communication between <strong>Minimally Verbal Autistic Children</strong> and <strong>Parents</strong> with Contextual Guidance and Card Recommendation</div>
       </div>
--- app/sections/Abstract.tsx
+++ app/sections/Abstract.tsx
@@ -1,12 +1,18 @@
 import { Section } from "../components/Section"
 import { ArxivIcon, GithubIcon } from "../components/icons"
-import { NewspaperIcon } from "@heroicons/react/20/solid"
+import { NewspaperIcon, TrophyIcon } from "@heroicons/react/20/solid"
 
 export const Abstract = () => {
     return <Section title="Abstract" className="flex-1 pt-0 md:pt-12">
         <p>
         As minimally verbal autistic (MVA) children communicate with parents through few words and nonverbal cues, parents often struggle to encourage their children to express subtle emotions and needs and to grasp their nuanced signals. We present AACessTalk, a tablet-based, AI-mediated communication system that facilitates meaningful exchanges between an MVA child and a parent. AACessTalk provides real-time guides to the parent to engage the child in conversation and, in turn, recommends contextual vocabulary cards to the child. Through a two-week deployment study with 11 MVA child-parent dyads, we examine how AACessTalk fosters everyday conversation practice and mutual engagement. Our findings show high engagement from all dyads, leading to increased frequency of conversation and turn-taking. AACessTalk also encouraged parents to explore their own interaction strategies and empowered the children to have more agency in communication. We discuss the implications of designing technologies for balanced communication dynamics in parent-MVA child interaction.
         </p>
+
+        <div className="flex items-center gap-2 my-4">
+            <TrophyIcon className="w-6 h-6 text-pink-600"/>
+            <span className="font-semibold text-pink-600 text-xl">Best Paper Award at CHI 2025</span>
+        </div>
+
         <div className="flex flex-wrap gap-4 mt-4">
             <a className="icon-label-button bg-teal-500 pl-4" aria-disabled={true} href={"./public/aacesstalk-chi25.pdf"} target="_blank">
                 <NewspaperIcon className="w-6 h-6 text-white"/>
@@ -21,5 +27,6 @@ export const Abstract = () => {
                 <span>Source code</span>
             </a>
         </div>
+        
     </Section>
 }
